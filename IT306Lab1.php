5143b31194a601cbc3433dfaa9e2a1ea6891cfa1
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-02-15 16:32:44 +0300

    m

--- IT306Lab1.php
+++ IT306Lab1.php
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-
-    <form method="post" action="IT306Lab1.php" >
-          F  <input type="text" name="first" id="f"/></br>
-          S <input type="text" name="second" id="s"/></br>
-          T <input type="text" name="third" id="t"/></br>
-          <input type="submit" name="submit" value="Submit" />
-    </form>
-
-    <?php
-    $a=$_POST('first');
-    echo "First is " . $a;
-    ?>
-
-  </body>
-</html>
+<?php
+$a=$_POST('first');
+echo "First is " . $a;
+?>
