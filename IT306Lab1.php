56a580adbd3fef27c34d498543796cb999be6923
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-02-15 16:30:17 +0300

    n

--- IT306Lab1.php
+++ IT306Lab1.php
@@ -7,17 +7,15 @@
   <body>
 
     <form method="post" action="IT306Lab1.php" >
-          F  <input type="text" name="name" id="f"/></br>
-          S <input type="text" name="name" id="s"/></br>
-          T <input type="text" name="name" id="t"/></br>
+          F  <input type="text" name="first" id="f"/></br>
+          S <input type="text" name="second" id="s"/></br>
+          T <input type="text" name="third" id="t"/></br>
           <input type="submit" name="submit" value="Submit" />
     </form>
 
     <?php
-    $USER = "Serdar";
-    echo 'My username is ' .$_ENV["USER"] . '!';
-    $site_path_var = $_SERVER["SITE_HTMLROOT"];
-    echo $site_path_var;
+    $a=$_POST('first');
+    echo "First is " . $a;
     ?>
 
   </body>
