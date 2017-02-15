35549cd5f46e315a01613e2e7bf39fd81e87607f
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-02-15 16:45:02 +0300

    m

--- IT306Lab1.php
+++ IT306Lab1.php
@@ -1,9 +1,28 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-$a=$_POST['first'];
-$b=$_POST['second'];
-$c=$_POST['third'];
-$avg=($a+$b+$c)/3.0;
-echo "Average is $avg";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form method="post" action="IT306Lab1.php" >
+          F  <input type="text" name="first" id="first"/></br>
+          S <input type="text" name="second" id="s"/></br>
+          T <input type="text" name="third" id="t"/></br>
+          <input type="submit" name="submit" value="Submit" />
+    </form>
+    <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    $a=$_POST['first'];
+    $b=$_POST['second'];
+    $c=$_POST['third'];
+    $avg=($a+$b+$c)/3.0;
+    echo "Average is $avg";
+    ?>
+
+
+
+  </body>
+</html>
