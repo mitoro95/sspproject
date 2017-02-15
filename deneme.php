048377571f9c8ddc79f57c84df3ff540436a597b
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-02-15 10:53:30 +0300

    vv

--- deneme.php
+++ deneme.php
@@ -7,10 +7,10 @@
   <body>
     <?php
     ini_set('display_errors', 1);
-    define('NAME', "Serdar Vural");
+    define('NAME', 'Serdar Vural');
     $x = 10;
 
-    echo "The value of constant name is NAME <br>";
+    echo "The value of constant name is" .NAME "<br>";
     echo "X is $x";
      ?>
 
