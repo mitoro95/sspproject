c5790a5bf7c75042234efc1169b1ed8160f41531
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-02-15 16:26:14 +0300

    m

--- IT306Lab1.php
+++ IT306Lab1.php
@@ -5,6 +5,14 @@
     <title></title>
   </head>
   <body>
+
+    <form method="post" action="IT306Lab1.php" >
+          F  <input type="text" name="name" id="f"/></br>
+          S <input type="text" name="name" id="s"/></br>
+          T <input type="text" name="name" id="t"/></br>
+          <input type="submit" name="submit" value="Submit" />
+    </form>
+
     <?php
     $USER = "Serdar";
     echo 'My username is ' .$_ENV["USER"] . '!';
